96d57b91e3f94c9d6b86e8a25777ad60733a824b
Patrick Kelly <patomation@gmail.com>
2021-06-04 12:50:02 +0700

    fix(app): test added promise

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,15 +2,18 @@ import test from 'ava'
 import supertest from 'supertest'
 import { app } from './app'
 
-test('app get', (t) => {
-  supertest(app)
-    .get('/')
-    .expect('foobar')
-    .expect(200)
-    .end((err, res) => {
-      // If error throw error
-      if (err) throw err
-      // Else pass test
-    })
+test('app get', async (t) => {
+  await new Promise<void>((resolve) => {
+    supertest(app)
+      .get('/')
+      .expect('foobar')
+      .expect(200)
+      .end((err, res) => {
+        // If error throw error
+        if (err) throw err
+        // Else pass test
+        resolve()
+      })
+  })
   t.pass()
 })
