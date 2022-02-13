63026d06be0731680ff7d6671e31fc1c5a61c779
Patrick Kelly <patomation@gmail.com>
2022-02-12 16:22:57 -0800

    docker docker dockedr

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express'
+import cors from "cors"
 import { postJsonDataExample } from './api/postJsonDataExample'
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(cors({
+  origin: '*'
+}))
+
 app.get('/', (req, res) => {
-  res.status(200).send('foobar')
+  res.status(200).send({jason: 'foobar3'})
 })
 
 app.use(postJsonDataExample)
