b9840da99e9ba1bc469abd09221106f5d3c4dd03
Patrick Kelly <patomation@gmail.com>
2022-02-13 13:43:21 -0800

    feat(docker): multi-stage-dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,30 @@
-FROM node:14
-WORKDIR /usr/src/app
+# BASE NODE
+FROM node:16-slim AS base
+WORKDIR /app
 COPY package*.json ./
+
+# DEPENDENCIES
+FROM base AS dependencies
+# Install only production modules  --ignore-scripts will skip running prepare after install
+RUN npm install --only=production --ignore-scripts
+# Copy prod modules into a different directory
+RUN cp -R node_modules prod_node_modules
+# Now install everything... that is needed for dev
 RUN npm install
+
+# TEST
+FROM dependencies AS test
+COPY . .
+RUN npm run test
+
+# Build
+FROM dependencies AS build
 COPY . .
 RUN npx tsc
 
-EXPOSE 8080
+# RELEASE
+FROM base AS release
+COPY --from=dependencies /app/prod_node_modules ./node_modules
+COPY --from=build /app/build ./build
+EXPOSE 80
 CMD [ "node", "./build/index.js" ]
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,39 @@
-import cors from "cors"
-import express from "express"
-import path from "path"
-import fs from "fs"
+import cors from 'cors'
+import express from 'express'
+import path from 'path'
+import fs from 'fs'
 
-const API = "api"
+const API = 'api'
 
 const app = express()
 const port = process.env.PORT || 80
 
-app.use(cors({
-  origin: '*'
-}))
+app.use(
+  cors({
+    origin: '*',
+  })
+)
 
 /***
  * Load up all the api files
  */
 async function main() {
-  const targetDirectory = path.join(__dirname, API);
+  const targetDirectory = path.join(__dirname, API)
   const fileNames = await fs.promises.readdir(targetDirectory)
-  await Promise.all(fileNames.map(async (fileName) => {
-    // Ignore test files and don't try to import them
-    if(!fileName.includes('test')) {
-      const filePath = `./${API}/${fileName}`
-      console.log(`importing ${filePath} as /${fileName.split(".")[0]}`)
-      const route = await import(filePath)
-      // assuming all routes files are exported as default with express get post patch ect
-      app.use(route?.default)
-    }
-  }))
+  await Promise.all(
+    fileNames.map(async (fileName) => {
+      // Ignore test files and don't try to import them - todo might not need to go into production
+      if (!fileName.includes('test')) {
+        const filePath = `./${API}/${fileName}`
+        console.log(`importing ${filePath} as /${fileName.split('.')[0]}`)
+        const route = await import(filePath)
+        // assuming all routes files are exported as default with express get post patch ect
+        app.use(route?.default)
+      }
+    })
+  )
   app.listen(port, () => {
     console.log('Server started on port: ' + port)
   })
 }
 main()
-
-
-
-
